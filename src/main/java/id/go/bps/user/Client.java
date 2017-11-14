c36b7f87faeaf00c7abf19e60e66fb3343c590a2
Soedomoto <soedomoto@gmail.com>
2017-11-14 16:14:18 +0700

    Update client example

--- src/main/java/id/go/bps/user/Client.java
+++ src/main/java/id/go/bps/user/Client.java
@@ -1,114 +1,58 @@
-/*
- * Copyright 2015, gRPC Authors All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package id.go.bps.user;
 
 import com.google.protobuf.Empty;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 import io.grpc.util.RoundRobinLoadBalancerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import resolver.AtomixNameResolverFactory;
-
-import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-/**
- * A simple client that requests a greeting from the {@link Server}.
- */
+
 public class Client {
-    private static final Logger logger = Logger.getLogger(Client.class.getName());
+    private static final Logger logger = LoggerFactory.getLogger(Client.class.getName());
 
-    private final ManagedChannel channel;
-    private final PositionServiceGrpc.PositionServiceStub stub;
-
-    /** Construct client connecting to HelloWorld server at {@code host:port}. */
-    public Client(String host, int port) {
-        this(ManagedChannelBuilder.forAddress(host, port)
-                // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
-                // needing certificates.
-                .usePlaintext(true)
-                .build());
-    }
-
-    /** Construct client for accessing RouteGuide server using the existing channel. */
-    Client(ManagedChannel channel) {
-        this.channel = channel;
-        stub = PositionServiceGrpc.newStub(channel);
-    }
-
-    public void shutdown() throws InterruptedException {
-        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
-    }
-
-    /** Say hello to server. */
-    public void greet(String name) {
-        logger.info("Will try to greet " + name + " ...");
-        Iterator<Position> response;
-        try {
-            stub.list(Empty.newBuilder().build(), new StreamObserver<Position>() {
-                @Override
-                public void onNext(Position position) {
-                    logger.info("Greeting: " + position.getName());
-                }
-
-                @Override
-                public void onError(Throwable throwable) {
-
-                }
-
-                @Override
-                public void onCompleted() {
-
-                }
-            });
-        } catch (StatusRuntimeException e) {
-            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
-            return;
-        }
-    }
-
-    /**
-     * Greet server. If provided, the first element of {@code args} is the name to use in the
-     * greeting.
-     */
-    public static void main(String[] args) throws Exception {
+    private static ManagedChannel atomixChannel(String host, int port, String registryName) {
         ManagedChannel channel = ManagedChannelBuilder
-                .forTarget("atomix://localhost:8701/service-user")
+                .forTarget(String.format("atomix://%s:%d/%s", host, port, registryName))
                 .nameResolverFactory(new AtomixNameResolverFactory())
                 .loadBalancerFactory(RoundRobinLoadBalancerFactory.getInstance())
                 .usePlaintext(true)
                 .build();
+        return channel;
+    }
+
+    private static ManagedChannel directChannel(String host, int port) {
+        ManagedChannel channel = ManagedChannelBuilder
+                .forAddress(host, port)
+                .usePlaintext(true)
+                .build();
+        return channel;
+    }
+
+    public static void main(String[] args) throws Exception {
+        ManagedChannel channel = atomixChannel("localhost", 8701, "service-user");
 
         for(int i=0; i<100; i++) {
-//        Client client = new Client("localhost", 50051);
-            Client client = new Client(channel);
-            try {
-      /* Access a service running on the local machine on port 50051 */
-                String user = "world";
-                if (args.length > 0) {
-                    user = args[0]; /* Use the arg as the name to greet if provided */
+            PositionServiceGrpc.PositionServiceStub stub = PositionServiceGrpc.newStub(channel);
+            stub.list(Empty.newBuilder().build(), new StreamObserver<Position>() {
+                @Override
+                public void onNext(Position position) {
+                    logger.info("Position : {}", position);
                 }
-                client.greet(user);
-            } finally {
 
-            }
+                @Override
+                public void onError(Throwable throwable) {
+                    logger.error("Error : {}", throwable);
+                }
+
+                @Override
+                public void onCompleted() {
+                    logger.info("Complete");
+                }
+            });
         }
 
         channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
